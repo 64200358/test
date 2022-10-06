df5e329acdc4ef9c69fbaf371529a3404e54f7e8
64200358 <64200358@kmitl.ac.th>
2022-10-06 12:29:41 +0700

    Update form.php

--- form.php
+++ form.php
@@ -1,13 +1,3 @@
-<form neme="form" method="post" action="save.php" enctype="multipart/form-data">
-<label>Title:</label><input type="text"name="rank_title" /><br/>
-<label>Name:</label><input type="text"name="rank_name" /><br/>
-<label>Min:</label><input type="text"name="rank_min"/>
+
 <label>special:</label>
-<input type="radio"name="rank_special"id="rank_special1" value="1"checked="checked"/>
-<label for="rank_special1">yes</label>
-<input type="radio"name="rank_special"id="rank_special2"value="0"/>
-<label for="rank_special2">no</label><br/>
-<label>image:</label>
-<input type="file"name="rank_image"/><br/>
-<input type="submit" name="save" value="save" />
-<input name="reset" type="reset" value="clear"   />
+
